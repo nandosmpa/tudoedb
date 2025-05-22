d7d19b9cf95ea15338c7cd96b7d1c07facbc3d2d
Ian Marques <iandaniel.vm@gmail.com>
2025-05-22 18:33:54 +0000

    Iniciar disciplina com dois exemplos introdutórios
    Os exemplos focam no entendimento de orientação a objetos, sem preocupação com o pleno funcionamento do código neste estágio inicial.

---
+++ Aluno.java
@@ -0,0 +1,9 @@
+public class Aluno {
+    private int matricula;
+    private String nome;
+    private double notas;
+
+    public void adicionarNota(notas) {}
+    public void calcularMedia() {}
+    public void verificarAprovacao() { return 0; }
+}
---
+++ Produto.java
@@ -0,0 +1,9 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public void vender(quantidade) {}
+    public void reporEstoque(quantidade) {}
+    public void consultarEstoque() { return 0; }
+}
